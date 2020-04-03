9ad49f291afd74557371f611a0cb55e4818883a2
İsmail Namdar <namdari@itu.edu.tr>
2020-04-03 03:13:29 +0300

    styling(navbar): linting

--- src/views/NavBar.js
+++ src/views/NavBar.js
@@ -1,17 +1,26 @@
 import React from "react";
 import PropTypes from "prop-types";
+import classNames from "classnames";
 import "./NavBar.sass";
 
-const NavBar = ({ children }) => {
-  return <div className="NavBar">{children}</div>;
+const NavBar = ({ spaceBetween, children }) => {
+  return (
+    <div
+      className={classNames("NavBar", { NavBar__spaceBetween: spaceBetween })}
+    >
+      {children}
+    </div>
+  );
 };
 
 NavBar.propTypes = {
   children: PropTypes.node,
+  spaceBetween: PropTypes.bool,
 };
 
 NavBar.defaultProps = {
   children: <></>,
+  spaceBetween: false,
 };
 
 export default NavBar;
